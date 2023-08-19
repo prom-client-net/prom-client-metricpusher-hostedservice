25660663483c66e3b71ec8c82544ca2d0ade1d30
Serge K <cmt@phnx47.net>
2023-08-19 11:52:03 +0700

    test: add test with default interval

--- tests/MetricPusherServiceTests.cs
+++ tests/MetricPusherServiceTests.cs
@@ -6,6 +6,20 @@ namespace Prometheus.Client.MetricPusher.HostedService.Tests;
 public class MetricPusherServiceTests
 {
     [Fact]
+    public async Task WithDefaultInterval_PushMetricPeriodically()
+    {
+        var metricPusherMock = Substitute.For<IMetricPusher>();
+        var metricPusherService = new MetricPusherService(metricPusherMock);
+        var canellationToken = Arg.Any<CancellationToken>();
+
+        await metricPusherService.StartAsync(canellationToken);
+        await Task.Delay(TimeSpan.FromSeconds(1), canellationToken);
+        await metricPusherService.StopAsync(canellationToken);
+
+        await metricPusherMock.Received(3).PushAsync();
+    }
+
+    [Fact]
     public async Task WithGivenInterval_PushMetricPeriodically()
     {
         var metricPusherMock = Substitute.For<IMetricPusher>();
