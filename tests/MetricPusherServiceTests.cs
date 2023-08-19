a751e42ec2bb703a03d9798f39ef2054242c596e
Serge K <cmt@phnx47.net>
2023-08-19 11:49:20 +0700

    refactor: to file-scoped namespace

--- tests/MetricPusherServiceTests.cs
+++ tests/MetricPusherServiceTests.cs
@@ -1,22 +1,21 @@
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Prometheus.Client.MetricPusher.HostedService.Tests
+namespace Prometheus.Client.MetricPusher.HostedService.Tests;
+
+public class MetricPusherServiceTests
 {
-    public class MetricPusherServiceTests
+    [Fact]
+    public async Task WithGivenInterval_PushMetricPeriodically()
     {
-        [Fact]
-        public async Task WithGivenInterval_PushMetricPeriodically()
-        {
-            var metricPusherMock = Substitute.For<IMetricPusher>();
-            var metricPusherService = new MetricPusherService(metricPusherMock, TimeSpan.FromSeconds(1));
-            var canellationToken = Arg.Any<CancellationToken>();
+        var metricPusherMock = Substitute.For<IMetricPusher>();
+        var metricPusherService = new MetricPusherService(metricPusherMock, TimeSpan.FromSeconds(1));
+        var canellationToken = Arg.Any<CancellationToken>();
 
-            await metricPusherService.StartAsync(canellationToken);
-            await Task.Delay(TimeSpan.FromSeconds(1), canellationToken);
-            await metricPusherService.StopAsync(canellationToken);
+        await metricPusherService.StartAsync(canellationToken);
+        await Task.Delay(TimeSpan.FromSeconds(1), canellationToken);
+        await metricPusherService.StopAsync(canellationToken);
 
-            await metricPusherMock.Received(3).PushAsync();
-        }
+        await metricPusherMock.Received(3).PushAsync();
     }
 }
--- tests/ServiceCollecttionExtensionsTests.cs
+++ tests/ServiceCollecttionExtensionsTests.cs
@@ -1,42 +1,41 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Prometheus.Client.MetricPusher.HostedService.Tests
+namespace Prometheus.Client.MetricPusher.HostedService.Tests;
+
+public class ServiceCollecttionExtensionsTests
 {
-    public class ServiceCollecttionExtensionsTests
+    [Fact]
+    public void AddMetricPusherService_WithNullMetricPusher_ThrowsArgumentNullException()
     {
-        [Fact]
-        public void AddMetricPusherService_WithNullMetricPusher_ThrowsArgumentNullException()
-        {
-            var servicesCollection = Substitute.For<IServiceCollection>();
+        var servicesCollection = Substitute.For<IServiceCollection>();
 
-            Action act = () => servicesCollection.AddMetricPusherService(null, TimeSpan.FromSeconds(1));
+        Action act = () => servicesCollection.AddMetricPusherService(null, TimeSpan.FromSeconds(1));
 
-            act.Should().Throw<ArgumentNullException>();
-        }
+        act.Should().Throw<ArgumentNullException>();
+    }
 
-        [Fact]
-        public void AddMetricPusherService_WithZeroTimeInterval_ThrowsArgumentOutOfRangeException()
-        {
-            var servicesCollection = Substitute.For<IServiceCollection>();
-            var metricPusher = Substitute.For<IMetricPusher>();
+    [Fact]
+    public void AddMetricPusherService_WithZeroTimeInterval_ThrowsArgumentOutOfRangeException()
+    {
+        var servicesCollection = Substitute.For<IServiceCollection>();
+        var metricPusher = Substitute.For<IMetricPusher>();
 
-            Action act = () => servicesCollection.AddMetricPusherService(metricPusher, TimeSpan.Zero);
+        Action act = () => servicesCollection.AddMetricPusherService(metricPusher, TimeSpan.Zero);
 
-            act.Should().Throw<ArgumentOutOfRangeException>();
-        }
+        act.Should().Throw<ArgumentOutOfRangeException>();
+    }
 
-        [Fact]
-        public void AddMetricPusherService_WithValidParameterValues_AddsMetricPusherServiceInServiceCollection()
-        {
-            var servicesCollection = new ServiceCollection();
-            var metricPusher = Substitute.For<IMetricPusher>();
+    [Fact]
+    public void AddMetricPusherService_WithValidParameterValues_AddsMetricPusherServiceInServiceCollection()
+    {
+        var servicesCollection = new ServiceCollection();
+        var metricPusher = Substitute.For<IMetricPusher>();
 
-            servicesCollection.AddMetricPusherService(metricPusher, TimeSpan.FromSeconds(1));
-            var provider = servicesCollection.BuildServiceProvider();
-            var service = provider.GetRequiredService<IHostedService>();
+        servicesCollection.AddMetricPusherService(metricPusher, TimeSpan.FromSeconds(1));
+        var provider = servicesCollection.BuildServiceProvider();
+        var service = provider.GetRequiredService<IHostedService>();
 
-            service.Should().BeOfType<MetricPusherService>();
-        }
+        service.Should().BeOfType<MetricPusherService>();
     }
 }
